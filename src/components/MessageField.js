6abf40bb6aa9c4d59e1055123a9157d6dd756503
ko-suke24 <kosuke0322@gmail.com>
2020-12-29 10:35:59 +0900

    display new message

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -18,7 +18,7 @@ const MessageField = ({ inputEl, name, setText, text }) => {
         if (text === '') return;
 
         if (e.key === 'Enter') {
-          pushMessage({ name: 'はむさん', text });
+          pushMessage({ name, text });
           setText('');
           e.preventDefault();
         }
